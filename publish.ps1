33572428dcb09ad7ce68a17076f67f0a98cd75ee
fc1943s <fc1943s@users.noreply.github.com>
2023-03-10 14:11:49 +0000

    deploy: e5f5c7b9248eed0367aabe40d965b4012d948367

--- publish.ps1
+++ publish.ps1
@@ -1,5 +1,5 @@
 $a = fleek sites deploy
+Write-Output $a
 $a = $a | grep "Site IPFS CID"
 $a = $a | ForEach-Object { $_.ToString().Split(' ')[4] }
-
 fleek ipns publish k51qzi5uqu5dhcucwwd09yny2mnx4uz14ws3gdvfpea5s1nescnx2kufio1x0c $a
