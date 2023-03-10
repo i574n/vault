e5b927df78f5009f12f569258306084ecb9b41ce
fc1943s <root@fc1943s.dev>
2023-03-10 11:00:20 -0300

    data

--- publish.ps1
+++ publish.ps1
@@ -1,1 +1,5 @@
-fleek sites deploy | grep "Site IPFS CID" | ForEach-Object { $_.ToString().Split('')[4] } | xargs fleek ipns publish k51qzi5uqu5dhcucwwd09yny2mnx4uz14ws3gdvfpea5s1nescnx2kufio1x0c
+$a = fleek sites deploy
+$a = $a | grep "Site IPFS CID"
+$a = $a | ForEach-Object { $_.ToString().Split(' ')[4] }
+
+fleek ipns publish k51qzi5uqu5dhcucwwd09yny2mnx4uz14ws3gdvfpea5s1nescnx2kufio1x0c $a
