d33a69a70db3e57715b3dd588af9258a62bea3e1
프리마 <fc1943s@pm.me>
2023-08-29 02:42:57 -0300

    data

--- README.md
+++ README.md
@@ -8,6 +8,6 @@
 
 <https://i574n.github.io/vault>
 
-<https://ipfs.io/ipns/k51qzi5uqu5dhcucwwd09yny2mnx4uz14ws3gdvfpea5s1nescnx2kufio1x0c>
+<https://ipfs.io/ipns/k51qzi5uqu5dmg05gnqt02fmke0xnobp0psyve0dsgcq0bjso97u1btto2hbfc>
 
 [![깃허브 코드스페이스](https://github.com/codespaces/badge.svg)](https://github.com/codespaces/new?hide_repo_select=true&ref=main&repo=602361649)
--- scripts/publish-fleek.ps1
+++ scripts/publish-fleek.ps1
@@ -5,12 +5,15 @@ Set-Location $ScriptDir
 $ErrorActionPreference = "Stop"
 
 
-npm i -g @fleekxyz/cli
+npm i -g @fleekxyz/cli@dev
 
 Set-Location ..
+
+Write-Output "Location: $(Get-Location)"
 
 $a = fleek sites deploy
 Write-Output $a
 $a = $a | grep "Site IPFS CID"
-$a = $a | ForEach-Object { $_.ToString().Split(' ')[4] }
-fleek ipns publish k51qzi5uqu5dhcucwwd09yny2mnx4uz14ws3gdvfpea5s1nescnx2kufio1x0c $a
+$cid = $a | ForEach-Object { $_.ToString().Split(' ')[4] }
+Write-Output "CID: $cid"
+fleek ipns publish --name k51qzi5uqu5dmg05gnqt02fmke0xnobp0psyve0dsgcq0bjso97u1btto2hbfc --hash $cid
