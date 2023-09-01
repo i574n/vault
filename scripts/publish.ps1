e40880073735a188cf860145605cb6eeacdd827c
fc1943s <fc1943s@users.noreply.github.com>
2023-09-01 10:23:26 +0000

    deploy: af6af8084029a62e012f4362f0a8400efa9ee80e

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -36,22 +36,63 @@ Get-ChildItem -Path ../dist -Recurse -Force | Where-Object { $_.Name.StartsWith(
 
 New-Item -ItemType Directory -Path "../target/scripts" -Force | Out-Null
 
-$url = "https://i574n.github.io/polyglot/scripts/core.ps1"
-$path = "../target/scripts/core.ps1"
-Invoke-WebRequest $url -OutFile $path -ErrorAction SilentlyContinue
+$retryCount = 0
+Write-Host "## 1"
+while ($retryCount -lt 5) {
+    if ($retryCount -gt 0) {
+        Write-Host "Retry $retryCount / Error: '$Error'"
+    }
+    $Error.Clear()
+    Write-Host "## 2"
 
-. ../target/scripts/core.ps1
 
-$fileName = "DirTreeHtml$(GetExecutableSuffix)"
-$cidUrl = "https://i574n.github.io/polyglot/apps/dir-tree-html/target/dist/$fileName.cid"
-Write-Output "cidUrl: '$cidUrl'"
-$cid = Invoke-WebRequest $cidUrl -ErrorAction SilentlyContinue
-$cid = $cid.ToString().Trim()
-Write-Output "cid: '$cid'"
+    $url = "https://i574n.github.io/polyglot/scripts/core.ps1"
+    $path = "../target/scripts/core.ps1"
+    try {
+        Invoke-WebRequest $url -OutFile $path
+        . ../target/scripts/core.ps1
+    } catch {
+        Write-Host "Exception: '$_' / Error: '$Error'"
+        $retryCount++
+        continue
+    }
 
-$dwebUrl = "https://dweb.link/ipfs/$cid"
-$path = "../target/$fileName"
-Invoke-WebRequest $dwebUrl -OutFile $path -ErrorAction SilentlyContinue
+    $fileName = "DirTreeHtml$(GetExecutableSuffix)"
+    $cidUrl = "https://i574n.github.io/polyglot/apps/dir-tree-html/target/dist/$fileName.cid"
+    Write-Output "cidUrl: '$cidUrl'"
+    try {
+        $cid = Invoke-WebRequest $cidUrl
+    } catch {
+        Write-Host "Exception: '$_' / Error: '$Error'"
+        $retryCount++
+        continue
+    }
+    $cid = $cid.ToString().Trim()
+    Write-Output "cid: '$cid'"
+
+    $dwebUrl = "https://dweb.link/ipfs/$cid"
+    $path = "../target/$fileName"
+    try {
+        Invoke-WebRequest $dwebUrl -OutFile $path
+    } catch {
+        Write-Host "Exception: '$_' / Error: '$Error'"
+    }
+
+    Write-Host "## 3"
+
+    if ($Error.Count -eq 0) {
+        Write-Host "## 3.1"
+        break
+    }
+
+    Write-Host "## 4"
+    $retryCount++
+}
+Write-Host "## 5"
+if ($Error.Count -gt 0) {
+    throw "Failed to download files / Error: '$Error'"
+    exit 1
+}
 
 { chmod +x $path } | Invoke-Block -Linux
 
