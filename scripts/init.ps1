df9da2fce3f3a708c9d98bee6cd482ff6c8d761e
프리마 <fc1943s@pm.me>
2024-02-12 16:14:32 -0400

    data

--- scripts/init.ps1
+++ scripts/init.ps1
@@ -5,12 +5,6 @@ Set-Location $ScriptDir
 $ErrorActionPreference = "Stop"
 
 
-{ sudo apt-get update } | Invoke-Block -Linux -Distro ubuntu
-{ sudo apt install -y texlive-xetex } | Invoke-Block -Linux -Distro ubuntu
-
-{ cargo +nightly install crowbook } | Invoke-Block -OnError Continue
-
-
 Set-Location (New-Item -ItemType Directory -Path "../.." -Force)
 git clone --recurse-submodules https://github.com/i574n/polyglot.git
 Set-Location polyglot
@@ -21,3 +15,10 @@ pwsh scripts/init.ps1
 pwsh apps/dir-tree-html/build.ps1
 
 Set-Location $ScriptDir
+
+. ../../polyglot/scripts/core.ps1
+
+{ sudo apt-get update } | Invoke-Block -Linux -Distro ubuntu
+{ sudo apt install -y texlive-xetex } | Invoke-Block -Linux -Distro ubuntu
+
+{ cargo +nightly install crowbook } | Invoke-Block -OnError Continue
