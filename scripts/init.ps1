a604bb0035e9b32a89a87cf111cdacb607d2d746
프리마 <fc1943s@pm.me>
2024-04-09 17:58:41 -0400

    tests

--- scripts/init.ps1
+++ scripts/init.ps1
@@ -1,25 +1,32 @@
 param(
+    $fast,
     $ScriptDir = $PSScriptRoot
 )
 Set-Location $ScriptDir
 $ErrorActionPreference = "Stop"
 
 
-Set-Location (New-Item -ItemType Directory -Path "../.." -Force)
-git clone --recurse-submodules https://github.com/i574n/polyglot.git
-Set-Location polyglot
-git pull
+if (!$fast) {
+    Set-Location (New-Item -ItemType Directory -Path "../.." -Force)
+    git clone --recurse-submodules https://github.com/i574n/polyglot.git
+    Set-Location polyglot
+    git pull
 
-pwsh scripts/init.ps1
+    pwsh scripts/init.ps1
+} else {
+    Set-Location ../../polyglot
+}
 
-pwsh apps/builder/build.ps1
-pwsh apps/parser/build.ps1
-pwsh lib/fsharp/build.ps1
-pwsh apps/dir-tree-html/build.ps1
+. ./scripts/core.ps1
+
+pwsh apps/builder/build.ps1 -fast 1
+pwsh apps/parser/build.ps1 -fast 1
+pwsh lib/fsharp/build.ps1 -fast 1
+pwsh apps/dir-tree-html/build.ps1 -fast 1
 
 Set-Location $ScriptDir
 
-. ../../polyglot/scripts/core.ps1
+pwsh dep_hangulize.ps1 -fast 1
 
 { sudo apt-get update } | Invoke-Block -Linux -Distro ubuntu
 { sudo apt install -y texlive-xetex } | Invoke-Block -Linux -Distro ubuntu
--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -1,4 +1,6 @@
 param(
+    $fast,
+    $SkipSync,
     $ScriptDir = $PSScriptRoot
 )
 Set-Location $ScriptDir
@@ -6,77 +8,180 @@ $ErrorActionPreference = "Stop"
 . ../../polyglot/scripts/core.ps1
 
 
-Remove-Item ../dist -Recurse -Force -ErrorAction Ignore
+if (!$SkipSync) {
+    Remove-Item ../dist -Recurse -Force -ErrorAction Ignore
 
-rsync -av `
-    --exclude '.git' `
-    --exclude '.history' `
-    --exclude '.vscode' `
-    --exclude 'file_cache' `
-    --exclude 'target' `
-`
-    --include 'LICENSE' `
-    --include '*.editorconfig' `
-    --include '*.edn' `
-    --include '*.epub' `
-    --include '*.gitignore' `
-    --include '*.json' `
-    --include '*.html' `
-    --include '*.md' `
-    --include '*.pdf' `
-    --include '*.ps1' `
-    --include '*.toml' `
-`
-    --include '*/' `
-    --exclude '*' `
-    --no-links `
-    --prune-empty-dirs `
-    ../ `
-    ../dist/
+    rsync -av `
+        --exclude '.git' `
+        --exclude '.history' `
+        --exclude '.vscode' `
+        --exclude 'file_cache' `
+        --exclude 'target' `
+        --exclude 'deps' `
+    `
+        --include 'LICENSE' `
+        --include '*.editorconfig' `
+        --include '*.edn' `
+        --include '*.epub' `
+        --include '*.gitignore' `
+        --include '*.json' `
+        --include '*.html' `
+        --include '*.md' `
+        --include '*.pdf' `
+        --include '*.ps1' `
+        --include '*.toml' `
+    `
+        --include '*/' `
+        --exclude '*' `
+        --no-links `
+        --prune-empty-dirs `
+        ../ `
+        ../dist/
 
-Get-ChildItem -Path ../dist -Recurse -Force | Where-Object { $_.Name.StartsWith(".") } | ForEach-Object {
-    Rename-Item -Path $_.FullName -NewName "_$($_.Name)"
-}
-
-git fetch --prune --all --verbose
-
-$ghPages = "../target/gh-pages"
-if (!(Test-Path $ghPages)) {
-    git clone $(git ls-remote --get-url origin) --branch gh-pages "$ghPages"
-    if (!(Test-Path $ghPages)) {
-        exit 1
+    Get-ChildItem -Path ../dist -Recurse -Force | Where-Object { $_.Name.StartsWith(".") } | ForEach-Object {
+        Move-Item $_.FullName "$($_.FullName | Split-Path)/_$($_.Name)" -Force
     }
 }
 
-$distRoot = (Resolve-Path ../dist/).Path
+$targetDir = (Resolve-Path "../target/gh-pages").Path
+
+Write-Output "targetDir: $targetDir"
+
+git clone $(git ls-remote --get-url origin) --branch gh-pages "$targetDir"
+if (!(Test-Path $targetDir)) {
+    exit 1
+}
+Set-Location $targetDir
+if (!$fast) {
+    git reset --hard
+    git clean -fd
+}
+git pull --rebase -Xtheirs
+Set-Location $ScriptDir
 
 Get-ChildItem -Path ../dist -Recurse -Force `
-| Where-Object { $_.Extension -eq ".md" } `
+| Where-Object { $_.Extension -eq ".md" -and !$_.Name.EndsWith(".hangul.md") } `
 | ForEach-Object {
-    $relative = $_.FullName.Replace($distRoot, '').Replace("\", "/")
+    . ../../polyglot/scripts/core.ps1
 
-    $originHash = git ls-tree --format='%(objectname)' origin/gh-pages ../$relative
-    $localGitHash = git hash-object $_.FullName
+    $distDir = (Resolve-Path ../dist).Path
+    $targetDir = (Resolve-Path "../target/gh-pages").Path
+    # $hangulize = $(Resolve-Path "../deps/hangulize/cmd/hangulize/hangulize$(GetExecutableSuffix)").Path
+
+    $relativePath = $_.FullName.Replace($distDir, "").Replace("\", "/")
+    $relativePathWithoutExt = $relativePath.Substring(0, $relativePath.LastIndexOf("."))
+    $fullPath = "$distDir$relativePath"
+    $originHash = git ls-tree --format='%(objectname)' origin/gh-pages ../$relativePath
+    $localGitHash = git hash-object $fullPath
+
+    $files = @(
+        "$relativePath.html",
+        "$relativePath.pdf",
+        "$relativePath.epub"
+        # "$relativePathWithoutExt.hangul.md"
+    )
 
     if ($localGitHash -ne $originHash) {
-        Write-Output "$($_.FullName)"
-        crowbook --single "$($_.FullName)" --output "$($_.FullName).html" --to html --set rendering.num_depth 6 html.css.add ''' body { color: #e8e6e3; background-color: #202324; } a { color: #989693; } '''
-        crowbook --single "$($_.FullName)" --output "$($_.FullName).pdf" --to pdf --set rendering.num_depth 6 html.css.add ''' body { color: #e8e6e3; background-color: #202324; } a { color: #989693; } '''
-        crowbook --single "$($_.FullName)" --output "$($_.FullName).epub" --to epub --set rendering.num_depth 6 html.css.add ''' body { color: #e8e6e3; background-color: #202324; } a { color: #989693; } '''
-    } else {
-        $files = @(
-            @("$ghPages/$relative.html", "../dist/$relative.html"),
-            @("$ghPages/$relative.pdf", "../dist/$relative.pdf"),
-            @("$ghPages/$relative.epub", "../dist/$relative.epub")
-        )
+        $hash1 = (Get-FileHash $fullPath -Algorithm SHA256).Hash
+        $targetPath = "$targetDir$relativePath"
+        if (Test-Path $targetPath) {
+            $hash2 = (Get-FileHash $targetPath -Algorithm SHA256).Hash
+        }
 
+        if (!$hash2 -or $hash1 -ne $hash2) {
+            Write-Output "relativePathWithoutExt: $relativePathWithoutExt / relativePath: $relativePath / fullPath: $fullPath / targetPath: $targetPath / originHash: $originHash / localGitHash: $localGitHash / hash1: $hash1 / hash2: $hash2"
+            crowbook --single "$fullPath" --output "$fullPath.html" --to html --set rendering.num_depth 6 html.css.add ''' body { color: #e8e6e3; background-color: #202324; } a { color: #989693; } '''
+            crowbook --single "$fullPath" --output "$fullPath.pdf" --to pdf --set rendering.num_depth 6 html.css.add ''' body { color: #e8e6e3; background-color: #202324; } a { color: #989693; } '''
+            crowbook --single "$fullPath" --output "$fullPath.epub" --to epub --set rendering.num_depth 6 html.css.add ''' body { color: #e8e6e3; background-color: #202324; } a { color: #989693; } '''
+
+            # Write-Output "#1"
+            # $lines = Get-Content $fullPath
+            # Write-Output "#2"
+            # $linesTrim = $lines | Where-Object { $_.Trim() -ne "" }
+            # Write-Output "#3"
+
+            # $chunkSize = 10
+            # $linesChunks = $linesTrim | Group-Object -Property { [math]::Floor($i++ / $chunkSize) }
+            # Write-Output "#4"
+
+            # $encoding = [System.Text.Encoding]::UTF8
+
+            # $_i = 0
+            # $linesChunks | ForEach-Object {
+            #     $linesChunk = $_
+
+            #     $process = New-Object System.Diagnostics.Process
+            #     $ProcessStartInfoParam = [ordered]@{
+            #         FileName = $hangulize.Replace("\", "/")
+            #         Arguments = "por-br"
+            #         RedirectStandardOutput = $true
+            #         RedirectStandardInput = $true
+            #         UseShellExecute = $false
+            #         CreateNoWindow = $true
+            #         StandardOutputEncoding = $encoding
+            #     }
+            #     $ProcessStartInfo = New-Object -TypeName 'System.Diagnostics.ProcessStartInfo' -Property $ProcessStartInfoParam
+            #     $process.StartInfo = $ProcessStartInfo
+            #     Write-Output "#5"
+            #     $start = $process.Start() | Out-Null
+            #     Write-Output "#6 / start: $start"
+
+            #     $i = 0
+            #     $linesChunk.Group | ForEach-Object {
+            #         $text = $_.Trim()
+            #         # only keep letters
+            #         $text = $text
+            #         Write-Output "#6.1 / i: $i / text: $text"
+            #         $process.StandardInput.WriteLine($text)
+            #         $i++
+            #     }
+            #     Write-Output "#7"
+            #     $process.StandardInput.Close()
+            #     Write-Output "#8"
+            #     $exit = $process.WaitForExit(5000)
+            #     Write-Output "#9"
+
+            #     if ($exit) {
+            #         $output = $process.StandardOutput.ReadToEnd()
+            #         $output = $output.Split("`n")
+
+            #         $i = 0
+            #         $n = 0
+            #         $lines | ForEach-Object {
+            #             if ($_.Trim() -ne "") {
+            #                 $lines[$i + ($_i * $chunkSize)] = $output[$i - $n]
+            #             } else {
+            #                 $n++
+            #             }
+            #             $i++
+            #         }
+            #     } else {
+            #         $process.Kill()
+            #     }
+
+            #     $_i++
+            # }
+
+            # Set-Content -Encoding Default "$distDir$relativePathWithoutExt.hangul.md" $lines
+
+            Copy-Item $fullPath $targetPath
+            foreach ($file in $files) {
+                $targetFile = "$targetDir$file"
+                $sourceFile = "$distDir$file"
+                if (Test-Path $sourceFile) {
+                    Copy-Item $sourceFile $targetFile
+                }
+            }
+        }
+    } else {
         foreach ($file in $files) {
-            if ((Test-Path $file[0]) -and !(Test-Path $file[1])) {
-                Copy-Item $file[0] $file[1]
+            $targetFile = "$targetDir$file"
+            $distFile = "$distDir$file"
+            if ((Test-Path $targetFile) -and !(Test-Path $distFile)) {
+                Copy-Item $targetFile $distFile
             }
         }
     }
 }
 
-$fileName = "DirTreeHtml$(GetExecutableSuffix)"
-{ . ../../polyglot/apps/dir-tree-html/dist/$fileName --dir ../dist --html ../dist/index.html } | Invoke-Block
+{ . ../../polyglot/apps/dir-tree-html/dist/DirTreeHtml$(GetExecutableSuffix) --dir ../dist --html ../dist/index.html } | Invoke-Block
