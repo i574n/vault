b11a4cbf52b0c8e86954d298e6fa4eefc2394372
프리마 <fc1943s@pm.me>
2024-05-14 12:38:36 -0400

    tests

--- scripts/init.ps1
+++ scripts/init.ps1
@@ -26,9 +26,9 @@ if (!$fast) {
 { pwsh ../../polyglot/apps/dir-tree-html/build.ps1 -fast 1 } | Invoke-Block
 
 { sudo apt-get update } | Invoke-Block -Linux -Distro ubuntu
-{ sudo apt install -y texlive-xetex } | Invoke-Block -Linux -Distro ubuntu
+{ sudo apt install -y texlive-xetex pandoc fonts-nanum-coding } | Invoke-Block -Linux -Distro ubuntu
 
-{ cargo +nightly install crowbook } | Invoke-Block -OnError Continue
+{ cargo +nightly install --git https://github.com/crowdagger/crowbook.git --locked } | Invoke-Block -OnError Continue
 
 { pwsh ./dep_hangulize.ps1 -fast 1 } | Invoke-Block
 
